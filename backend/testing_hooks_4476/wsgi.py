904f9cff4a7887174d03e379e9f7a1664352082f
Crowdbotics <team@crowdbotics.com>
2019-06-10 07:02:10 +0000

    initial commit

---
+++ backend/testing_hooks_4476/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testing_hooks_4476 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_hooks_4476.settings')
+
+application = get_wsgi_application()
